c88899901cdb5200b547d4f0e897a3203f3c13e2
Irfan Anshari <irfananshr.id@gmail.com>
2025-01-18 22:20:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,26 @@ const roles = [
     "Boti", "Fomo", "Wota", "Tobrut", "Chill Guy", "Gooner", "Hawk Tuah", 
     "Rizzler", "Alpha", "Impostor", "Smartest Human Exist", "Mario Mushroom", 
     "Domain Expansion", "Furry", "Bacon Exploiter", "Bloxfruit", "Jamaican", 
-    "African", "Albanian Bodyguard", "Grimace Shake", "Miguel O'Hara", 
-    "Fat Sigma", "Skinny Fuckwit", "Big Chungus", "Don Pollo", 
+    "African", "Albanian Bodyguard", "Miguel O'Hara", 
+    "Fat Sigma", "Skinny Fuckwit", "Big Chungus", "Don Pollo's Friend", 
     "Australian Laugh", "Serbian Fury", "Jonkler", "Lion Pizza Chicken", 
     "Sharting", "Lithuanian Respect", "Irish Smirk", "Balkan Rage", 
     "Greek Stare", "German Stare", "Brazilian Fire", "Japanese Bow", 
-    "Nigerian Sprint", "Masturbate Master", "Pakistani Backflip", "Kai Cenat", 
-    "Thanos Gauntlet", "Angry Chinese Burp", "Ancient Egyptian Testicle Slap"
+    "Nigerian Sprinter", "Masturbate Master", "Pakistani Backflip", "Kai Cenat", 
+    "Thanos", "Angry Chinese", "Ancient Egyptian Testicle Slapper"
 ];
 const generateButton = document.getElementById("generateButton");
 const nameInput = document.getElementById("nameInput");
 const roleResult = document.getElementById("roleResult");
 const loading = document.getElementById("loading");
+
+function toggleBodyScroll(disable) {
+    if (disable) {
+        document.body.classList.add("no-scroll");
+    } else {
+        document.body.classList.remove("no-scroll");
+    }
+}
 
 generateButton.addEventListener("click", function() {
     const name = nameInput.value;
@@ -26,6 +34,7 @@ generateButton.addEventListener("click", function() {
     } else {
         roleResult.textContent = ""; 
         loading.style.display = "flex";
+        toggleBodyScroll(true);
 
         setTimeout(function() {
             const randomRole = roles[Math.floor(Math.random() * roles.length)];
@@ -33,6 +42,7 @@ generateButton.addEventListener("click", function() {
             
             nameInput.value = "";
             loading.style.display = "none";
+            toggleBodyScroll(false);
         }, 3000);
     }
 });
