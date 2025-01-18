934cdc2e24e61681179919df36b04880e5fe58d0
Irfan Anshari <irfananshr.id@gmail.com>
2025-01-18 20:39:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,17 @@
 const roles = [
-    "Artis",
-    "Chef",
-    "Suki",
-    "Rasis",
-    "Cabul",
-    "Sepuh",
-    "Sigma",
-    "Pedo",
-    "Jomok",
-    "Cakep",
-    "Kroco",
-    "Mewing",
-    "Klemer",
-    "Wibu",
-    "Tukang Bully",
-    "Pintar",
-    "Bego",
-    "Skibidi",
-    "Jawir",
-    "Cina",
-    "Kaya Raya",
-    "Burik",
-    "Boti",
-    "Fomo",
-    "Wota",
-    "Tobrut",
-    "Chill Guy",
-    "Gooner",
-    "Hawk Tuah",
-    "Rizzler",
-    "Alpha",
-    "Impostor",
-    "Smartest Human Exist",
-    "Mario Mushroom",
-    "Domain Expansion",
-    " Furry",
-    "Bacon Exploiter",
-    "Bloxfruit",
-    "Jamaican",
-    "African",
-    "Albanian Bodyguard",
-    "Grimace Shake",
-    "Miguel O'Hara",
-    "Fat Sigma",
-    "Skinny Fuckwit",
-    "Big Chungus",
-    "Don Pollo",
-    "Australian Laugh",
-    "Serbian Fury",
-    "Jonkler",
-    "Lion Pizza Chicken",
-    "Sharting",
-    "Lithuanian Respect",
-    "Irish Smirk",
-    "Balkan Rage",
-    "Greek Stare",
-    "German Stare",
-    "Brazilian Fire",
-    "Japanese Bow",
-    "Nigerian Sprint",
-    "Masturbate Master",
-    "Pakistani Backflip",
-    "Kai Cenat",
-    "Thanos Gauntlet",
-    "Angry Chinese Burp",
-    "Ancient Egyptian Testicle Slap"
+    "Artis", "Chef", "Suki", "Rasis", "Cabul", "Sepuh", "Sigma", "Pedo", 
+    "Jomok", "Cakep", "Kroco", "Mewing", "Klemer", "Wibu", "Tukang Bully", 
+    "Pintar", "Bego", "Skibidi", "Jawir", "Cina", "Kaya Raya", "Burik", 
+    "Boti", "Fomo", "Wota", "Tobrut", "Chill Guy", "Gooner", "Hawk Tuah", 
+    "Rizzler", "Alpha", "Impostor", "Smartest Human Exist", "Mario Mushroom", 
+    "Domain Expansion", "Furry", "Bacon Exploiter", "Bloxfruit", "Jamaican", 
+    "African", "Albanian Bodyguard", "Grimace Shake", "Miguel O'Hara", 
+    "Fat Sigma", "Skinny Fuckwit", "Big Chungus", "Don Pollo", 
+    "Australian Laugh", "Serbian Fury", "Jonkler", "Lion Pizza Chicken", 
+    "Sharting", "Lithuanian Respect", "Irish Smirk", "Balkan Rage", 
+    "Greek Stare", "German Stare", "Brazilian Fire", "Japanese Bow", 
+    "Nigerian Sprint", "Masturbate Master", "Pakistani Backflip", "Kai Cenat", 
+    "Thanos Gauntlet", "Angry Chinese Burp", "Ancient Egyptian Testicle Slap"
 ];
 const generateButton = document.getElementById("generateButton");
 const nameInput = document.getElementById("nameInput");
@@ -84,6 +31,7 @@ generateButton.addEventListener("click", function() {
             const randomRole = roles[Math.floor(Math.random() * roles.length)];
             roleResult.textContent = `${name}, role kamu adalah: ${randomRole}`;
             
+            nameInput.value = ""; // Hapus input setelah hasil ditampilkan
             loading.style.display = "none";
         }, 3000);
     }
@@ -92,8 +40,6 @@ generateButton.addEventListener("click", function() {
 nameInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         generateButton.click();
-
-        
         nameInput.blur();
     }
 });
